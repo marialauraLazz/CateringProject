06f0aabfa3a5270dbdbaa827b0f41a9d579a22ae
mari_lazz <lazlala2001@gmail.com>
2022-05-22 15:41:06 +0200

    modifica chef

--- catering-Project/src/main/java/it/uniroma3/siw/catering/controller/ChefController.java
+++ catering-Project/src/main/java/it/uniroma3/siw/catering/controller/ChefController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import it.uniroma3.siw.catering.model.Buffet;
 import it.uniroma3.siw.catering.model.Chef;
 import it.uniroma3.siw.catering.service.ChefService;
 import it.uniroma3.siw.catering.validator.ChefValidator;
@@ -56,9 +55,31 @@ public class ChefController {
 	}
 	
 	@GetMapping("/toDeleteChef/{id}")
-	public String deletePersona(@PathVariable("id") Long id, Model model) {
+	public String deletechef(@PathVariable("id") Long id, Model model) {
 		chefService.removeById(id);
 		return "redirect:/chefs";
+	}
+	
+	@GetMapping("/toEditChef/{id}")
+	public String getChefEdited(@PathVariable("id") Long id, Model model) {
+		Chef chef= chefService.findById(id);
+		model.addAttribute("chef", chef);
+		return "editChef.html";
+	}
+	
+	@PostMapping("/toEditChef/{id}")
+	public String editChef(@Valid @ModelAttribute("chef") Chef chef, BindingResult bindingResults, @PathVariable("id") Long id, Model model) {
+		Chef chefDaModificare= chefService.findById(id);
+		chefValidator.validate(chef, bindingResults);
+		if(!bindingResults.hasErrors()) {
+			chefDaModificare.setNome(chef.getNome());
+			chefDaModificare.setCognome(chef.getCognome());
+			chefDaModificare.setNazionalita(chef.getNazionalita());
+			chefDaModificare.setBuffet(chef.getBuffet());
+			chefService.save(chefDaModificare);
+			return "redirect:/chefs";
+		}
+		return "editChef.html";
 	}
 	
 }
